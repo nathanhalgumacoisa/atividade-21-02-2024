41d51d7ffa6c46637563d94386d277729592c99e
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2024-02-21 16:56:41 -0300

    eeeeeee

--- App.jsx
+++ App.jsx
@@ -1,41 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import React from 'react'
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <View className={`flex flex-row justify-between items-center mb-6`}>
-        <TextInput
-        placeholder="Digite uma nova tarefa"
-        className={`border-b border-b-black p-2 w-3/4`}
-        onChangeText={setTask}
-        value={task}
-        />
-        <TouchableOpacity
-        className={`bg-blue-500 p-2 rounded-md`}
-        >
-        <Text className={`text-white`}>Adicionar</Text>
-        </TouchableOpacity>
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <div>App</div>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  form: {
-    display: "flex",
-    flexdirection:"row",
-    justifyContent:"space-between",
-    alignItems:"center",
-    marginBottom:"20",
-
-  }
-});
+export default App
